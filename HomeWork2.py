55e9c14ef4814c49f104799944b067b9322e2209
William Higgins <wjhiggin@syr.edu>
2023-02-08 19:15:05 -0500

    Transfer Files

---
+++ HomeWork2.py
@@ -0,0 +1,21 @@
+#Outline
+    #This is how I plan to create this algorthim
+#Input
+    #csv File
+#output
+    #Info that tells a story about the data
+        #Histogram 
+
+# %%
+    #This is how you create a cell
+    #For importing all needed libraries
+import plotnine
+import pandas as pd
+import csv
+print("Loading Successful")
+# %%
+df = pd.read_csv(r'csv_files\data-storyteller (1).csv')
+print(df)
+# %%
+#test: Creating a scatterplot graph
+plot = plotnine(data=surveys_complete, mapping = plotnine.aes(x='Section'))
